cf2fec06bf2f540edec767454dc7e5fa066343c3
arianbe <arian.berisha.ext@doodle.com>
2023-10-18 20:05:01 +0200

    android bug fix

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 /* eslint-disable import/namespace */
 import { configureStore } from "@reduxjs/toolkit";
+import countryReducer from "./reducers/countryReducer";
 import homeReducer from "./reducers/homeReducer";
 
 export const store = configureStore({
   reducer: {
     home: homeReducer,
+    country: countryReducer,
   },
 });
 
